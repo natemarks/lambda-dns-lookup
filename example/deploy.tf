ff5e2f2e75b7306aae5bf12776e52ab97ec5ffa2
Nate Marks <npmarks@gmail.com>
2021-02-28 10:19:25 -0500

    Reorganize examples

---
+++ example/deploy.tf
@@ -0,0 +1,32 @@
+# Example should be modified for your purposes and copied to deploy.tf
+module "aws_lambda_monitor" {
+  source = "github.com/natemarks/tf-aws-lambda-monitor?ref=v0.0.11"
+
+  aws_account_id   = "0123456789012"
+  aws_region       = "us-east-1"
+  function_name    = "lambda-dns-lookup-monitor"
+  handler_name     = "lambda-dns-lookup"
+  function_runtime = "go1.x"
+  tracing_mode     = "PassThrough"
+  source_bucket    = "bucket_where_i_keep_my_lambda_app"
+  source_key       = "lambda-dns-lookup-monitor/lambda-dns-lookup.zip"
+
+  custom_tags = {
+    terraform   = "true"
+    environment = "production"
+  }
+  environment_variables = {
+    DEBUG          = "false"
+    RANDOM_FAILURE = "false"
+    LOOKUPS = jsonencode(
+      [
+        {
+          "Target" : "rpapi.cts.imprivata.com",
+          "ExpectedResponses" : 4
+        }
+      ]
+    )
+  }
+  schedule_expression      = "rate(1 minute)"
+  schedule_expression_desc = "Every minute"
+}
